1994328a2fbc5c3bca86a3210e21050f43d3f524
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2018-03-17 14:58:28 +0600

    First commit
    added the structure

---
+++ LOJ-1333.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const int N = 1e5 + 5;
+
+int main() {
+        #ifdef _OFFLINE_
+                freopen("in.txt", "r", stdin);
+        #endif /// _OFFLINE_
+
+
+
+        return 0;
+}
